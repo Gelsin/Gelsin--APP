6c1036448776a8a806a47415e5e7869b947b48e1
Ali Zeynalov <ali.yasharoglu@gmail.com>
2017-02-22 00:27:35 +0200

    Produkt sehifesi statik olaraq tamamlandi.

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,46 +1,70 @@
 import React, {Component} from 'react';
-import {ListView, TextInput, View, Image, Platform, TouchableOpacity, ScrollView, StyleSheet,Animated} from "react-native";
-import {Header,Container,Body,Text,Icon,Right,Left,Item,Button,Input,Content,Card,CardItem,Thumbnail} from "native-base";
+import {ListView, TextInput, View, Image, Platform, TouchableOpacity, ScrollView, StyleSheet,Animated,ActivityIndicator} from "react-native";
+import {Header,Container,Body,Text,Icon,Right,Left,Item,Button,Input,Content,Card,CardItem} from "native-base";
 import {Actions} from 'react-native-router-flux';
 import Product from './common/ProductItem';
+
+
 export default class Products extends Component {
+
 
 
     constructor(props) {
         super(props);
         this.state = {
+            SubCategories: null,
+            isLoading: true
         }
     }
+
     getProducts()
     {
 
     }
 
+    getSubCategories()
+    {
+        CatId = this.props.data;
+        var Url = "http://gelsin.az/app/api/categories/" + CatId;
+        return fetch(Url, {method: "GET"})
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({SubCategories: responseData.category.childs});
+                this.setState({isLoading: false});
+            })
+            .done();
+        console.log(this.setState.SubCategories);
+
+    }
+
     componentWillMount()
     {
-        this.getProducts();
-        console.log(this.props.data);
+        this.getSubCategories();
     }
 
     render() {
-
+        if(this.state.SubCategories === null) {
+            return <Header  action={()=>Actions.products(this.props.data)} newsLayoutButton={true} />
+        }
 
         const css = {
             "subCategoryRow": {
-                "height": 40,
+                "height": 48,
                 "backgroundColor": '#524656',
                 "paddingTop": 0,
                 "marginLeft": 0,
                 "marginBottom": 8,
-                "flexDirection": "row"
-
+                "flexDirection": "row",
+                alignItems: 'center'
             },
             "subCategoryBtn": {
                 "backgroundColor": '#524656',
                 borderWidth: 1,
-                borderColor: '#FFF',
+                borderColor: '#E5DDCB',
                 height: 30,
-                margin: 5
+                margin: 5,
+                paddingLeft: 15,
+                paddingRight: 15
             },
             "searchBar": {
                 "backgroundColor": 'green',
@@ -55,114 +79,68 @@ export default class Products extends Component {
             }}
 
         return (
+
+
             <Container>
+                <ActivityIndicator animating={this.state.isLoading}></ActivityIndicator>
                 <Header style={{"backgroundColor": '#524656'}}>
                     <Left>
-                        <Icon name="ios-menu" style={{color: '#E5DDCB'}}></Icon>
+                    <Button transparent>
+                        <Icon name="ios-arrow-round-back-outline" style={{color: '#E5DDCB'}}></Icon>
+                    </Button>
                     </Left>
                     <Body>
                     <Text style={{letterSpacing: 0.5,color: '#E5DDCB'}}>GƏLSİN</Text>
                     </Body>
                     <Right>
-                        <Text style={{top: -12,color: '#E5DDCB',fontSize: 14}}>12.50</Text>
+                        <Text style={{top: -12, color: '#E5DDCB',fontSize: 14}}>12.50</Text>
                         <Button transparent onPress={()=>Actions.cart()}>
                             <Icon name="ios-cart-outline" style={{color: '#E5DDCB'}}></Icon>
                         </Button>
                     </Right>
                 </Header>
-                    <Item style={{backgroundColor: '#EB7B59',justifyContent: 'center',margin: 0}}>
+                    <Item style={{backgroundColor: '#EB7B59',justifyContent: 'center',margin: 0, paddingLeft: -40, borderWidth: 0}}>
                         <Icon name="search" style={{color: '#E5DDCB',marginLeft: 6}}></Icon>
                         <Input placeholder="search something" placeholderTextColor="rgba(255, 255, 255, 0.6)" style={{textAlign: 'center',color: '#FFF'}}/>
                     </Item>
 
                 <View horizontal={true} style={css.subCategoryRow}>
-                   <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                    <Button rounded style={css.subCategoryBtn}>
-                        <Text>Juice</Text>
-                    </Button>
-                    <Button rounded style={css.subCategoryBtn}>
-                        <Text>Water</Text>
-                    </Button>
-                    <Button rounded style={css.subCategoryBtn}>
-                        <Text>Ice tea</Text>
-                    </Button>
-                    <Button  rounded style={css.subCategoryBtn}>
-                        <Text>Soda</Text>
-                    </Button>
-                    <Button  rounded  style={css.subCategoryBtn}>
-                        <Text>Energy Drink</Text>
-                    </Button>
-                   </ScrollView>
+                    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                {this.state.SubCategories.map((category,i) => {
+                  return  <Button rounded style={css.subCategoryBtn}>
+                                <Text style={{color: '#E5DDCB'}}>{category.name}</Text>
+                            </Button>
+                })}
+                    </ScrollView>
+
                 </View>
 
                 <Content>
-                <Card style={{flexDirection: 'row'}}>
+                    {this.state.SubCategories.map((category,i) => {
+                        return <Card style={{borderWidth: 0,marginLeft: 16,shadowOpacity: 0}}>
+                            <Text style={{borderWidth: 0,marginBottom: 14, color: '#EB7B59'}}>{category.name}</Text>
                     <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}  pagingEnabled={true}
-                                style={{borderWidth: 2, borderColor: 'green',height: 140}} >
-                    <Product name="ice tea" thumbnail="http://gelsin.az/app/api/product/image/27" price="12.50 ₼"/>
+                                style={{flexDirection: 'row'}}>
+                        <Product name="Ice Tea" thumbnail="http://gelsin.az/app/api/product/image/27" price="12.50 ₼"/>
 
-                        <Product name="ice tea" thumbnail="http://gelsin.az/app/api/product/image/28" price="12.50 ₼"/>
-
-
-                        <Product name="ice tea" thumbnail="http://gelsin.az/app/api/product/image/29" price="12.50 ₼"/>
+                        <Product name="Cola" thumbnail="http://gelsin.az/app/api/product/image/32" price="12.50 ₼"/>
 
 
-                        <Product name="ice tea" thumbnail="http://gelsin.az/app/api/product/image/27" price="12.50 ₼"/>
+                        <Product name="Cola" thumbnail="http://gelsin.az/app/api/product/image/30" price="12.50 ₼"/>
+
+
+                        <Product name="Cola" thumbnail="http://gelsin.az/app/api/product/image/31" price="12.50 ₼"/>
+
+
+                        <Product name="Fanta" thumbnail="http://gelsin.az/app/api/product/image/33" price="12.50 ₼"/>
+
+
+                        <Product name="Cola" thumbnail="http://gelsin.az/app/api/product/image/28" price="12.50 ₼"/>
 
                     </ScrollView>
                 </Card>
 
-                <Card style={{flexDirection: 'row'}}>
-                    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}  pagingEnabled={true}
-                                style={{borderWidth: 2, borderColor: 'green',height: 140}} >
-                        <Product name="ice tea" thumbnail="http://gelsin.az/app/api/product/image/27" price="12.50 ₼"/>
-
-
-                        <Product name="ice tea" thumbnail="http://gelsin.az/app/api/product/image/27" price="12.50 ₼"/>
-
-
-                        <Product name="ice tea" thumbnail="http://gelsin.az/app/api/product/image/27" price="12.50 ₼"/>
-                    </ScrollView>
-                </Card>
-
-                <Card style={{flexDirection: 'row'}}>
-                    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}  pagingEnabled={true}
-                                style={{borderWidth: 2, borderColor: 'green',height: 140}} >
-                        <Product name="ice tea" thumbnail="http://gelsin.az/app/api/product/image/27" price="12.50 ₼"/>
-
-
-                        <Product name="ice tea" thumbnail="http://gelsin.az/app/api/product/image/27" price="12.50 ₼"/>
-
-
-                        <Product name="ice tea" thumbnail="http://gelsin.az/app/api/product/image/27" price="12.50 ₼"/>
-                    </ScrollView>
-                </Card>
-
-                <Card style={{flexDirection: 'row'}}>
-                    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}  pagingEnabled={true}
-                                style={{borderWidth: 2, borderColor: 'green',height: 140}} >
-                        <Product name="ice tea" thumbnail="http://gelsin.az/app/api/product/image/27" price="12.50 ₼"/>
-
-
-                        <Product name="ice tea" thumbnail="http://gelsin.az/app/api/product/image/27" price="12.50 ₼"/>
-
-
-                        <Product name="ice tea" thumbnail="http://gelsin.az/app/api/product/image/27" price="12.50 ₼"/>
-                    </ScrollView>
-                </Card>
-
-                <Card style={{flexDirection: 'row'}}>
-                    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}  pagingEnabled={true}
-                                style={{borderWidth: 2, borderColor: 'green',height: 140}} >
-                        <Product name="ice tea" thumbnail="http://gelsin.az/app/api/product/image/27" price="12.50 ₼"/>
-
-
-                        <Product name="ice tea" thumbnail="http://gelsin.az/app/api/product/image/27" price="12.50 ₼"/>
-
-
-                        <Product name="ice tea" thumbnail="http://gelsin.az/app/api/product/image/27" price="12.50 ₼"/>
-                    </ScrollView>
-                </Card>
+                    })}
                 </Content>
 
             </Container>
--- src/components/common/ProductItem.js
+++ src/components/common/ProductItem.js
@@ -1,16 +1,16 @@
 import React, {Component} from 'react';
 import { View, TouchableOpacity} from "react-native";
-import {Text,CardItem,Thumbnail} from "native-base";
+import {Text,CardItem,Thumbnail,Button} from "native-base";
 
 export default class ProductItem extends Component {
 render()
 {
 return(
-    <CardItem horizontal={false} style={{flexDirection: 'column'}}>
+    <CardItem horizontal={false} style={{flexDirection: 'column',borderWidth: 1,borderRadius: 4,padding: 0,marginRight: 14, borderColor: '#E5DDCB'}}>
+        <Thumbnail square size={70} source={{uri: this.props.thumbnail }} style={{marginTop: 12, marginBottom: 12}}/>
         <Text>{this.props.name}</Text>
-        <Thumbnail square size={50} source={{uri: this.props.thumbnail }}/>
-        <TouchableOpacity><Text>Add To Cart</Text></TouchableOpacity>
-        <Text>{this.props.price}</Text>
+        <Button full style={{backgroundColor: '#EB7B59',height: 28,marginTop: 8, marginBottom: 8}}><Text>Add To Cart</Text></Button>
+        <Text style={{marginBottom: 8}}>{this.props.price}</Text>
     </CardItem>
 );
 
