ab09bf615784429b9dc0cdfac341de5748f4a433
Ali Zeynalov <ali.yasharoglu@gmail.com>
2017-03-13 19:54:46 +0300

    Adresses and Add Adress pages are ready.Edit address functionality will be added.

--- src/components/Addresses.js
+++ src/components/Addresses.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {View,Container, Header, Title, Content, Footer, FooterTab, Button, Body, Icon, H3, Text, Form, Item, Picker,Input,Card,CardItem,Left,Right} from 'native-base';
 import {Grid, Row, Col} from 'react-native-easy-grid';
+import {AsyncStorage} from 'react-native';
 import {Actions} from 'react-native-router-flux';
 import  ButtonRound  from './common/ButtonRound';
 
@@ -9,29 +10,71 @@ export default class Addresses extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            selected: 'key0',
-            selected1: 'rayon0'
+            token: '',
+            address: [],
+            selectedAddress: ''
         }
     }
 
-    onSeherChange (value: string) {
-        this.setState({
-            selected : value
-        });
-    }
+    // onSeherChange (value: string) {
+    //     this.setState({
+    //         selected : value
+    //     });
+    // }
+    //
+    // onRayonChange (value: string) {
+    //     this.setState({
+    //         selected1 : value
+    //     });
+    // }
 
-    onRayonChange (value: string) {
-        this.setState({
-            selected1 : value
-        });
-    }
 
-
-    componentDidMount()
+    goWithoutParam()
     {
+       var obj;
+       obj.addres_line=null;
+       obj.branch_addres_id=null;
+       Actions.newAddress(obj);
     }
+
+    getToken = async () => {
+        try {
+            var value = await AsyncStorage.getItem('@Gelsin:auth_user');
+            if (value !== null){
+                this.setState({token: value});
+                this.getAddresses(this.state.token);
+
+            } else {
+                console.log(value);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+
+    getAddresses(token) {
+        console.log("getting user addresses");
+
+        fetch('http://gelsin.az/app/api/addresses?token=' + token, {method: 'GET'})
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({address: responseJson.address},()=>console.log("ADRESLER: ",this.state.address));
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    componentWillMount() {
+        console.log("mounted");
+        this.getToken();
+    };
+
+
 
     render() {
+
         const styles = {
             header: {
                 backgroundColor: '#524656',
@@ -42,6 +85,8 @@ export default class Addresses extends Component {
         };
 
         return (
+
+
             <Container >
                 <Header style={styles.header}>
                     <Left style={{ flex: 1}}>
@@ -58,90 +103,26 @@ export default class Addresses extends Component {
                 <Content style={{marginTop: 30}}>
                         <H3 style={{color: '#EB7B59',fontFamily: 'SourceSansPro-Regular',paddingLeft: 14}}>Mövcud adresləriniz</H3>
 
-                        {/*<Button transparent>*/}
-                                {/*<Body>*/}
-                                    {/*<Text>Yasamal Is</Text>*/}
-                                    {/*<Text note>Adres 1 Yasamal Serifzade</Text>*/}
-                                {/*</Body>*/}
-                        {/*</Button>*/}
-
-                    <Card>
-                            <Button transparent>
-                            <Left>
-                                <Body>
-                                <Text>Ev 1</Text>
-                                <Text note>Yasamal,Serifzade 28, No 33</Text>
-                                </Body>
-                            </Left>
+                    {this.state.address.map((adres,i) => {
+                        return  <Card key={i}>
+                            <Button transparent onPress={()=>Actions.newAddress({adres})}>
+                                <Left>
+                                    <Body>
+                                    <Text>{adres.branch_address.street_name}</Text>
+                                    <Text note>{adres.address_line}</Text>
+                                    </Body>
+                                </Left>
                             </Button>
-                    </Card>
-                    <Card>
-                    <Button transparent>
-                            <Left>
-                                <Body>
-                                <Text>Ofis-Kurs 1</Text>
-                                <Text note>2 Sebayil,rayonu Neftciler pr. No 44</Text>
-                                </Body>
-                            </Left>
-                            </Button>
-                    </Card>
-                    <Card>
-                    <Button transparent>
-                            <Left>
-                                <Body>
-                                <Text>Ofis</Text>
-                                <Text note> Binəqədi,Süleyman Axundov küçəsi, No 12</Text>
-                                </Body>
-                            </Left>
-                            </Button>
-                    </Card>
-                    <Card>
-                    <Button transparent>
-                            <Left>
-                                <Body>
-                                <Text>Mekteb</Text>
-                                <Text note>Nəsimi, 42 Nömrəli orta məktəb, Otaq 62</Text>
-                                </Body>
-                            </Left>
-                            </Button>
-                    </Card>
+                        </Card>
+                    })}
 
 
-                    {/*<Item >*/}
-                            {/*<Button transparent>*/}
-                                {/*<Text style={{fontFamily: 'SourceSansPro-Bold'}}>ASDAS</Text>*/}
-                                {/*<Text style={{fontFamily: 'SourceSansPro-Regular'}}>Adres 1 Yasamal Serifzade</Text>*/}
-                            {/*</Button>*/}
-                        {/*</Item>*/}
-                        {/*<Item>*/}
-                            {/*<Button transparent>*/}
-                                {/*<Text>Adres 2 Nerimanov Tebriz kucesi</Text>*/}
-                            {/*</Button>*/}
-                        {/*</Item>*/}
-                        {/*<Item >*/}
-                            {/*<Button transparent>*/}
-                                {/*<Text>Adres 3 Bineqedi Suleyman Axundov kucesi</Text>*/}
-                            {/*</Button>*/}
-                        {/*</Item>*/}
-                        {/*<Item>*/}
-                            {/*<Button transparent>*/}
-                                {/*<Text>Adres 4 Sebayil rayonu Neftciler pr.</Text>*/}
-                            {/*</Button>*/}
-                        {/*</Item>*/}
                 </Content>
 
                 <Footer style={{"backgroundColor": '#524656'}}>
                     <FooterTab style={{flex: 1}}>
-                        {/*<View style={{flexDirection: 'row', flex: 1, justifyContent: 'space-between'}}>*/}
-                            {/*<Button transparent>*/}
-                                {/*<Text style={{fontFamily: 'SourceSansPro-Regular',marginTop: 10, color: '#FFF'}}>Cancel</Text>*/}
-                            {/*</Button>*/}
-                            {/*<Button rounded style={{backgroundColor: '#EB7B59',marginTop: 10,marginRight: 15, width: 120,height: 30,justifyContent: 'center'}}>*/}
-                                {/*<Text style={{fontFamily: 'SourceSansPro-Regular'}}>Save</Text>*/}
-                            {/*</Button>*/}
-                        {/*</View>*/}
-
-                        <Button transparent>
+                        <Button transparent onPress={()=>Actions.newAddress()}>
+                            <Icon name="ios-contacts-outline"/>
                             <Text style={{fontFamily: 'SourceSansPro-SemiBold', color: '#FFF'}}> + Yeni adres</Text>
                         </Button>
                     </FooterTab>
--- src/components/NewAddress.js
+++ src/components/NewAddress.js
@@ -1,34 +1,110 @@
 import React, {Component} from 'react';
-import {View,Container, Header, Title, Content, Footer, FooterTab, Button, Body, Icon, H3, Text, Form, Item, Picker,Input,Card,CardItem,Left,Right} from 'native-base';
+import {View,Container, Header, Title, Content, Footer, FooterTab, Button, Body, Icon, H3, Text, Form, Item, Input,Card,CardItem,Left,Right} from 'native-base';
 import {Grid, Row, Col} from 'react-native-easy-grid';
 import {Actions} from 'react-native-router-flux';
 import  ButtonRound  from './common/ButtonRound';
+import {AsyncStorage,Picker,AlertIOS,TextInput} from 'react-native';
 
 export default class NewAddress extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            selected: 'key0',
-            selected1: 'rayon0'
+            addressLine: this.props.adres? this.props.adres.address_line:null,
+            selectedBranchId: this.props.adres? this.props.adres.branch_address_id:null,
+
+            //addresLine: this.props.adres.address_line ? this.props.adres.address_line: null ,
+            //selectedBranchID: this.props.adres.branch_address_id ? this.props.adres.branch_address_id: null,
+            token: '',
+            user: String,
+            branchAddresses: []
+        }
+
+    }
+
+
+    getUser(token)
+    {
+        console.log("getting user");
+
+        fetch('http://gelsin.az/app/api/auth/user?token=' + token, {method: 'GET'})
+            .then((response) => response.json())
+            .then((responseJson) => {
+            if(responseJson.message == "authenticated user is customer") {
+                this.setState({user: responseJson.user}, () => console.log("User: ", this.state.user));
+            }
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+
+    }
+
+    getToken = async () => {
+        try {
+            var value = await AsyncStorage.getItem('@Gelsin:auth_user');
+            if (value != null){
+                this.setState({token: value},this.getUser(value),this.getBranchAddresses());
+            } else {
+                console.log("value null oldu.");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+
+    AddEditAdres()
+    {
+        if(this.props.adres == null)
+        {
+            //add Address
+            fetch("http://gelsin.az/app/api/address/add" ,{
+            method: 'POST',
+                headers: {
+                'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+
+            },
+                body: JSON.stringify({token: this.state.token, user_id: this.state.user.id, branch_address_id: this.state.selectedBranchID, address_line: this.state.addresLine})})
+                .then((response) => response.json())
+                .then((responseData) => {
+                })
+                .done();
+        }
+        else
+        {
+            //edit Address
+            console.log("EDIT ADRES");
+            console.log("TOKEN: ",this.state.token);
+            console.log("addressID: ",this.props.adres.id);
+            console.log("branchID: ",this.state.selectedBranchId);
+            console.log("Address Line: ",this.state.addressLine);
         }
     }
 
-    onSeherChange (value: string) {
-        this.setState({
-            selected : value
-        });
-    }
 
-    onRayonChange (value: string) {
-        this.setState({
-            selected1 : value
-        });
-    }
-
-
-    componentDidMount()
+    componentWillMount()
     {
+        this.getToken();
+        if(this.state.addressLine != null && this.state.branchAddresses !=null)
+        {
+            this.setState({addresLine: this.props.adres.address_line});
+            this.setState({selectedBranchID: this.props.adres.branch_address_id});
+        }
+    }
+
+
+    getBranchAddresses()
+    {
+        fetch('http://gelsin.az/app/api/branch/addresses', {method: 'GET'})
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({branchAddresses: responseJson.branches}, ()=>console.log("Branch adresleri: ",this.state.branchAddresses));
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     }
 
     render() {
@@ -41,72 +117,64 @@ export default class NewAddress extends Component {
             }
         };
 
-        return (
-            <Container >
-                <Header style={styles.header}>
-                    <Left style={{ flex: 1}}>
-                        <Button transparent>
-                            <Icon name='arrow-back' style={{color: '#FFF'}}/>
-                        </Button>
-                    </Left>
 
-                    <Body style={{ flex: 7}} >
-                    <Title style={{alignSelf: 'center',color: '#E5DDCB',paddingLeft: 20,fontFamily: 'SourceSansPro-Regular'}}>GƏLSİN</Title>
-                    </Body>
-                </Header>
-
-                <Content style={{marginTop: 30}}>
-                    <Form>
-                        <H3 style={{color: '#EB7B59',fontFamily: 'SourceSansPro-semiBold',paddingLeft: 14}}>New address</H3>
-
-                        <Item >
-                           <Picker
-                               iosHeader="Select one"
-                               mode="dropdown"
-                               selectedValue={this.state.selected}
-                               onValueChange={this.onSeherChange.bind(this)}>
-                               <Item label="Baku" value="key0" />
-                               <Item label="Sumqayit" value="key1" />
-                               <Item label="Gence" value="key2" />
-                           </Picker>
-                        </Item>
-
-                        <Item >
-                            <Picker
-                                iosHeader="Select one"
-                                mode="dropdown"
-                                selectedValue={this.state.selected1}
-                                onValueChange={this.onRayonChange.bind(this)}>
-                                <Item label="Yasamal" value="rayon0" />
-                                <Item label="Nizami" value="rayon1" />
-                                <Item label="Nəsimi" value="rayon2" />
-                                <Item label="Binəqədi" value="rayon3" />
-                            </Picker>
-                        </Item>
-
-                        <Item>
-                           <Input style={{left: 15,color: '#524656',paddingLeft: 0}}placeholder="adres açıqlaması"/>
-                        </Item>
-
-                        <Item>
-                            <Input style={{left: 15,color: '#524656',paddingLeft: 0}} placeholder="adres açıqlaması"/>
-                        </Item>
-                    </Form>
-                </Content>
-
-                <Footer style={{"backgroundColor": '#524656'}}>
-                    <FooterTab style={{flex: 1}}>
-                        <View style={{flexDirection: 'row', flex: 1, justifyContent: 'space-between'}}>
-                            <Button transparent>
-                                <Text style={{fontFamily: 'SourceSansPro-Regular',marginTop: 10, color: '#FFF'}}>Cancel</Text>
+            return (
+                <Container >
+                    <Header style={styles.header}>
+                        <Left style={{ flex: 1}}>
+                            <Button transparent onPress={()=>Actions.addresses()}>
+                                <Icon name='arrow-back' style={{color: '#FFF'}}/>
                             </Button>
-                            <Button rounded style={{backgroundColor: '#EB7B59',marginTop: 10,marginRight: 15, width: 120,height: 30,justifyContent: 'center'}}>
-                                <Text style={{fontFamily: 'SourceSansPro-Regular'}}>Save</Text>
-                            </Button>
-                        </View>
-                    </FooterTab>
-                </Footer>
-            </Container>
-        );
+                        </Left>
+
+                        <Body style={{ flex: 7}}>
+                        <Title
+                            style={{alignSelf: 'center',color: '#E5DDCB',paddingLeft: 20,fontFamily: 'SourceSansPro-Regular'}}>GƏLSİN</Title>
+                        </Body>
+                    </Header>
+
+                    <Content style={{marginTop: 30}}>
+                        <Form>
+                            <H3 style={{color: '#EB7B59',fontFamily: 'SourceSansPro-semiBold',paddingLeft: 14}}>Yeni adres</H3>
+
+                                <Picker
+                                    iosHeader="Select one"
+                                    mode="dropdown"
+                                    selectedValue={this.state.selectedBranchID}
+                                    onValueChange={(value)=>this.setState({
+                                    selectedBranchID : value},()=>console.log("Street Line: ",this.state.selectedBranchID))}>
+                                    {this.state.branchAddresses.map((address,i) =>
+                                         {
+                                             return <Picker.Item style={{fontFamily: 'SourceSansPro-Regular'}} label={address.street_name} value={address.id} key={i}/>}
+                                    )}
+                                </Picker>
+
+                            <Item>
+                                <Input
+                                    style={{left: 15,color: '#524656',paddingLeft: 0,fontFamily: 'SourceSansPro-Regular',height: 40 }}
+                                    placeholder="adres açıqlaması"
+                                    onChangeText={(Line) => this.setState({addresLine: Line},()=>console.log("adresLineS: ",this.state.addresLine))}/>
+                            </Item>
+
+                        </Form>
+                    </Content>
+
+                    <Footer style={{"backgroundColor": '#524656'}}>
+                        <FooterTab style={{flex: 1}}>
+                            <View style={{flexDirection: 'row', flex: 1, justifyContent: 'space-between'}}>
+                                <Button transparent>
+                                    <Text style={{fontFamily: 'SourceSansPro-Regular',marginTop: 10, color: '#FFF'}}>Cancel</Text>
+                                </Button>
+                                <Button rounded
+                                        style={{backgroundColor: '#EB7B59',marginTop: 10,marginRight: 15, width: 120,height: 30,justifyContent: 'center'}}
+                                        onPress={()=>this.AddEditAdres()}>
+                                    <Text style={{fontFamily: 'SourceSansPro-Regular'}}>Save</Text>
+                                </Button>
+                            </View>
+                        </FooterTab>
+                    </Footer>
+                </Container>
+            );
+
     }
 }
