4b78a50b9448e99261ae4449feb7e43dee52ee11
Ali Zeynalov <ali.yasharoglu@gmail.com>
2017-03-18 16:15:40 +0300

    Select Address selectedValue bug fix.

--- src/components/SelectAddress.js
+++ src/components/SelectAddress.js
@@ -10,19 +10,33 @@ export default class SelectAddress extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedBranchID: null,
-            branchAddresses: []
+            selectedBranchID: 1,
+            branchAddresses: [],
+            selectedValue: 1
         }
 
     }
 
     onSubmit() {
-        if (this.state.selectedBranchID == null || this.state.selectedBranchID == "0") {
-            Alert.alert("Xahiş edirik", "Adres seçimi edəsiniz");
-        }
-        else {
-            Actions.main({branchID: this.state.selectedBranchID});
+        Actions.main({branchID: this.state.selectedBranchID});
     }
+
+
+
+onValChange(val)
+{
+    console.log("onValueChange Value: ",val);
+    for (i=0; i<this.state.branchAddresses.length; i++)
+    {
+
+        if(this.state.branchAddresses[i].id == val)
+        {
+            console.log(this.state.branchAddresses[i]);
+            this.setState({selectedBranchID: this.state.branchAddresses[i].branch_id},console.log("branchID'si",this.state.selectedBranchID));
+            this.setState({selectedValue: this.state.branchAddresses[i].id},console.log("ID'si",this.state.selectedValue));
+        }
+    }
+
 }
 
 
@@ -39,7 +53,7 @@ export default class SelectAddress extends Component {
     }
 
 
-    componentDidMount()
+    componentWillMount()
     {
        this.getBranchAddresses();
     }
@@ -97,47 +111,6 @@ export default class SelectAddress extends Component {
 
 
         return (
-            /*<Container style={styles.container}>*/
-                // <Header style={styles.header}>
-                //         <Button transparent onPress={()=>Actions.account()}>
-                //             <Icon name="arrow-back" style={{color: '#E5DDCB'}}></Icon>
-                //         </Button>
-                //     <Body>
-                //     <Text style={{letterSpacing: 0.5,color: '#E5DDCB',fontFamily: 'SourceSansPro-semiBold'}}>ADRES</Text>
-                //     </Body>
-                // </Header>
-                //
-                //
-                //
-                //         <Picker
-                //             style={{width: 250, height: 300,margin: 80,backgroundColor: '#FFF'}}
-                //             iosHeader="Select one"
-                //             mode="dropdown"
-                //
-                //             prompt={"Adres"}
-                //             selectedValue={this.state.selectedBranchID}
-                //             value={this.state.selectedBranchID}
-                //             onValueChange={(value)=>this.setState({
-                //                     selectedBranchID : value},()=>console.log("Street Line: ",this.state.selectedBranchID))}>
-                //             <Picker.Item style={{fontFamily: 'SourceSansPro-Regular'}} label="" value={"0"} />
-                //             {this.state.branchAddresses.map((address,i) =>
-                //                 {
-                //                     return <Picker.Item style={{fontFamily: 'SourceSansPro-Regular'}} label={address.street_name} value={address.id} key={i}/>}
-                //             )}
-                //         </Picker>
-                //
-                //
-                // <Footer style={{"backgroundColor": '#524656',alignItems: 'center',justifyContent: 'center'}}>
-                //         <View>
-                //             <Button rounded
-                //                     style={{backgroundColor: '#EB7B59', width: 120,height: 30,justifyContent: 'center'}}
-                //                     onPress={()=>this.onSubmit()}>
-                //                 <Text style={{fontFamily: 'SourceSansPro-Regular'}}>Irəli</Text>
-                //             </Button>
-                //         </View>
-                // </Footer>
-            // </Container>
-
 
             <Container style={styles.container}>
                 <Header style={styles.header}>
@@ -152,15 +125,14 @@ export default class SelectAddress extends Component {
                             <Picker
                                 style={{width: 250, height: 300,margin: 80,backgroundColor: '#FFF'}}
                                 iosHeader="Select one"
-                                mode="dialog"
-                                selectedValue={this.state.selectedBranchID}
+                                mode="dropdown"
+                                selectedValue={this.state.selectedValue}
                                 value={this.state.selectedBranchID}
-                                onValueChange={(value)=>this.setState({
-                                    selectedBranchID : value},()=>console.log("Street Line: ",this.state.selectedBranchID))}>
-                                <Picker.Item style={{fontFamily: 'SourceSansPro-Regular'}} label="" value={"0"} />
+                                onValueChange={(value)=>this.onValChange(value)}>
                                 {this.state.branchAddresses.map((address,i) =>
                                     {
-                                        return <Picker.Item style={{fontFamily: 'SourceSansPro-Regular'}} label={address.street_name} value={address.branch_id} key={i}/>}
+
+                                        return <Picker.Item style={{fontFamily: 'SourceSansPro-Regular'}} label={address.street_name} value={address.id} key={i}/>}
                                 )}
                             </Picker>
 
