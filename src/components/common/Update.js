f3d69f51330ea0f790903f37aa365cb044b7fb40
Khalig Novruzli <khalig.novruzli@gmail.com>
2017-04-15 18:33:35 +0300

    ref to Update input and focus added

--- src/components/common/Update.js
+++ src/components/common/Update.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
-import {Modal, TouchableOpacity, TouchableWithoutFeedback} from 'react-native';
+import {Modal, TouchableOpacity, TouchableWithoutFeedback, TextInput} from 'react-native';
 import {Text, Input, View, Item} from 'native-base';
 import ButtonRound from './ButtonRound';
 
 
 export default class Update extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.focus = this.focus.bind(this);
+    // }
+
+    focus() {
+        // Explicitly focus the text input using the raw DOM API
+        this.refs.textInput.focus();
+    }
+
+
     render() {
         const styles = {
             label: {
@@ -24,6 +35,7 @@ export default class Update extends Component {
                 animationType='slide'
                 onRequestClose={() => {
                 }}
+                onShow={ ()=> this.refs.textInput.focus() }
             >
                 <TouchableOpacity
                     style={{backgroundColor: 'rgba(0,0,0,0.67)', justifyContent: "center", flex: 1}}
@@ -33,8 +45,9 @@ export default class Update extends Component {
                         <View style={{backgroundColor: '#fff', margin: 24, padding: 12}}>
                             <Text style={styles.label}>{this.props.label}</Text>
 
-                            <Input
+                            <TextInput
                                 style={styles.input}
+                                ref={'textInput'}
                                 value={this.props.value}
                                 onChangeText={this.props.onChange}
                             />
